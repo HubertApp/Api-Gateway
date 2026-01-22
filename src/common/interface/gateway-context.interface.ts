8cea1e43442096902f79d5e06cf94089ffe6468b
Boukada Adel <65848859+Leknif571@users.noreply.github.com>
2026-01-22 15:36:49 +0100

    feat : receive and validate token from headers

---
+++ src/common/interface/gateway-context.interface.ts
@@ -0,0 +1,8 @@
+export interface GatewayContext {
+  userId?: string;
+  userRole?: string;
+  userEmail?: string;
+  userPseudo?: string;
+  userAge?: number;
+  authState?: string;
+}
---
+++ src/common/interface/jwt.interface.ts
@@ -0,0 +1,7 @@
+export interface MyJwtPayload {
+  sub: string;
+  role: string;
+  email: string;
+  pseudo: string;
+  age: number;
+}
